902c1d8eb98d42ab043db97e9fc135326b8c878d
ikun <2076965410@qq.com>
2023-01-29 11:23:38 +0800

    提交第一次修改

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+let a = 100
 
 Vue.config.productionTip = false
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  lintOnSave:false,
+  transpileDependencies: true,
+  devServer: { //配置自动打开浏览器的地址
+    host: 'localhost',
+    port: 8080,
+  },
 })
